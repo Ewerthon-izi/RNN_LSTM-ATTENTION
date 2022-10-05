1a703823f133000e0c5b2d8f09115f51690e4641
Ewerthon-izi <72260672+Ewerthon-izi@users.noreply.github.com>
2022-10-04 21:20:16 -0300

    Add files via upload
    Add code to plot a image of distribuition of errors

---
+++ plotErrors.r
@@ -0,0 +1,13 @@
+library(ggstatsplot)
+library(gapminder)
+dados <- read.csv2("C:/Users/eweri/Desktop/TCC/erros11.csv", sep = ";", dec = ".") # nolint
+
+dplyr::glimpse(dados)
+
+plot(ggbetweenstats(
+  data  = dados,
+  x     = Estruturas,
+  y     = MAE,
+  type = "np",
+  title = "Distribuição dos erros nas estruturas"
+))
